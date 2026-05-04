d78bfbc018db7fe93acc5c0de1ca673250cfaaff
Georgina Focia <112983522+gfocia@users.noreply.github.com>
2026-05-03 18:46:52 -0700

    initial backend scaffolding commit

---
+++ fashion-backend/src/main/java/com/fashion_webapp/fashion_backend/FashionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fashion_webapp.fashion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionBackendApplication.class, args);
+	}
+
+}
